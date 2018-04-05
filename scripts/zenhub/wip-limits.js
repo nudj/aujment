0401e405f1e0ef93643b31923047534131800b2d
Nick Collings <nick@collingo.com>
2018-04-05 17:36:07 +0100

    Add combined column wip for release columns

--- scripts/zenhub/wip-limits.js
+++ scripts/zenhub/wip-limits.js
@@ -23,22 +23,47 @@ var observeDOM = (function () {
   }
 })()
 
-const WIPS = {
-  'This Cycle': {
+const camelCase = (words) => {
+  return words
+    .split(' ')
+    .map((word, index) => {
+      word = word.toLowerCase()
+      if (index) {
+        word = word[0].toUpperCase() + word.slice(1)
+      }
+      return word
+    })
+    .join('')
+}
+
+const limits = {
+  thisCycle: {
     pointMin: 10,
     pointMax: 40
   },
-  'In Progress': {
+  inProgress: {
     issueMin: 0,
     issueMax: 5
   },
-  'Code Review': {
+  codeReview: {
     issueMin: 0,
     issueMax: 3
   },
-  'Release': {
+  _release: {
     issueMin: 0,
     issueMax: 15
+  }
+}
+
+const WIPS = {
+  ...limits,
+  forReview: {
+    ...limits._release,
+    combinesWith: 'forProduction'
+  },
+  forProduction: {
+    ...limits._release,
+    combinesWith: 'forReview'
   }
 }
 
@@ -47,72 +72,74 @@ const bgRed = '#f4d4c8'
 
 // Observe a specific DOM element:
 observeDOM(document.getElementById('app'), function () {
-  const columns = [...document.querySelectorAll('.zhc-pipeline')]
-  if (columns.length) {
-    columns
-      .map(column => {
-        const issueCountElm = column.querySelector(
-          '.zhc-pipeline-header__issue-count'
-        )
-        const storyPointsElm = column.querySelector(
-          '.zhc-pipeline-header__story-points'
-        )
-        const columnTitleElm = column.querySelector(
-          '.zhc-pipeline-header__title'
-        )
+  const columnElms = [...document.querySelectorAll('.zhc-pipeline')]
+  if (columnElms.length) {
+    const {
+      list: columnList,
+      map: columnMap
+    } = columnElms.reduce((columnData, columnElm) => {
+      const issueCountElm = columnElm.querySelector('.zhc-pipeline-header__issue-count')
+      const storyPointsElm = columnElm.querySelector('.zhc-pipeline-header__story-points')
+      const columnTitleElm = columnElm.querySelector('.zhc-pipeline-header__title')
+      const issueCount = (issueCountElm && parseInt(issueCountElm.innerText.split(' ')[0], 10)) || 0
+      const storyPoints = (storyPointsElm && parseInt(storyPointsElm.innerText.split(' ')[1], 10)) || 0
+      const title = (columnTitleElm && columnTitleElm.innerText) || ''
+      const camelTitle = camelCase(title)
+      const column = {
+        columnElm,
+        issueCount,
+        storyPoints,
+        title,
+        camelTitle
+      }
 
-        const data = {}
+      columnData.list = (columnData.list || []).concat(column)
+      columnData.map = {
+        ...columnData.map,
+        [camelTitle]: column
+      }
+      return columnData
+    })
+    columnList.forEach(column => {
+      let error = false
+      const wips = WIPS[column.camelTitle]
 
-        if (issueCountElm) {
-          data.issueCount = parseInt(issueCountElm.innerText.split(' ')[0], 10)
-          data._issueCountElm = issueCountElm
+      if (wips) {
+        let issueCount = column.issueCount
+        let storyPoints = column.storyPoints
+
+        if (wips.combinesWith) {
+          const pairedColumn = columnMap[wips.combinesWith] || {}
+          issueCount += pairedColumn.issueCount || 0
+          storyPoints += pairedColumn.storyPoints || 0
         }
 
-        if (storyPointsElm) {
-          data.storyPoints = parseInt(storyPointsElm.innerText.split(' ')[1], 10)
-          data._storyPointsElm = storyPointsElm
+        if (wips.issueMin && issueCount < wips.issueMin) {
+          error = true
+        } else if (wips.issueMax && issueCount > wips.issueMax) {
+          error = true
+        } else if (wips.pointMin && storyPoints < wips.pointMin) {
+          error = true
+        } else if (wips.pointMax && storyPoints > wips.pointMax) {
+          error = true
+        } else {
+          error = false
         }
 
-        if (columnTitleElm) {
-          data.title = columnTitleElm.innerText
-          data._titleElm = columnTitleElm
+        if (error) {
+          column.columnElm.style.backgroundColor = fgRed
+        } else {
+          column.columnElm.backgroundColor = 'transparent'
         }
-
-        data._column = column
-
-        return data
-      })
-      .forEach(column => {
-        const wips = WIPS[column.title.split(':')[0]]
-        let error = false
-
-        if (wips) {
-          if (wips.issueMin && column.issueCount < wips.issueMin) {
-            error = true
-          } else if (wips.issueMax && column.issueCount > wips.issueMax) {
-            error = true
-          } else if (wips.pointMin && column.storyPoints < wips.pointMin) {
-            error = true
-          } else if (wips.pointMax && column.storyPoints > wips.pointMax) {
-            error = true
-          } else {
-            error = false
-          }
-
-          if (error) {
-            column._column.style.backgroundColor = fgRed
-          } else {
-            column._column.style.backgroundColor = 'transparent'
-          }
-        }
-      })
-    const cards = [...document.querySelectorAll('.zhc-issue-card')]
-    cards.forEach(card => {
-      const labels = [...card.querySelectorAll('.zhc-label')]
+      }
+    })
+    const cardElms = [...document.querySelectorAll('.zhc-issue-card')]
+    cardElms.forEach(cardElm => {
+      const labels = [...cardElm.querySelectorAll('.zhc-label')]
       const isExpedited = labels.some(label => label.textContent === 'EXPEDITED')
       if (isExpedited) {
-        card.style.backgroundColor = bgRed
-        card.style.borderColor = fgRed
+        cardElm.style.backgroundColor = bgRed
+        cardElm.style.borderColor = fgRed
       }
     })
   }
